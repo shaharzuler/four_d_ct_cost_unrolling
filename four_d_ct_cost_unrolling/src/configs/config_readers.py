7d6c668ec16bb605c359c426bf16767170d215be
shahar.zuler <shaharzuler@gmail.com>
2023-07-12 18:36:25 +0300

    adding json configs to package properly

--- four_d_ct_cost_unrolling/src/configs/config_readers.py
+++ four_d_ct_cost_unrolling/src/configs/config_readers.py
@@ -1,12 +1,21 @@
 import json
+import pkg_resources
 
 def get_default_backbone_config() -> dict:
-    with open("four_d_ct_cost_unrolling/four_d_ct_cost_unrolling/src/configs/backbone_default_configs.json") as file:
+    package_name = __name__.split('.')[0]
+    resource_path = '/'.join(('resources', 'four_d_ct_cost_unrolling','four_d_ct_cost_unrolling','src','configs','backbone_default_configs.json'))
+    file_path = pkg_resources.resource_filename(package_name, resource_path)
+    
+    with open(file_path) as file: #"four_d_ct_cost_unrolling/four_d_ct_cost_unrolling/src/configs/backbone_default_configs.json") as file:
         args = json.load(file)
     return args
 
 def get_default_w_constraints_config() -> dict:
-    with open("four_d_ct_cost_unrolling/four_d_ct_cost_unrolling/src/configs/w_constraints_default_configs.json") as file:
+    package_name = __name__.split('.')[0]
+    resource_path = '/'.join(('resources', 'four_d_ct_cost_unrolling','four_d_ct_cost_unrolling','src','configs','w_constraints_default_configs.json'))
+    file_path = pkg_resources.resource_filename(package_name, resource_path)
+
+    with open(file_path) as file: #open("four_d_ct_cost_unrolling/four_d_ct_cost_unrolling/src/configs/w_constraints_default_configs.json") as file:
         args = json.load(file)
     return args
 
