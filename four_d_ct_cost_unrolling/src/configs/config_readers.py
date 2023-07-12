0c224b87efac692a042a20d4403bfbc45e5aac35
shahar.zuler <shaharzuler@gmail.com>
2023-07-12 18:56:36 +0300

    addinf jsons

--- four_d_ct_cost_unrolling/src/configs/config_readers.py
+++ four_d_ct_cost_unrolling/src/configs/config_readers.py
@@ -3,7 +3,7 @@ import pkg_resources
 
 def get_default_backbone_config() -> dict:
     package_name = __name__.split('.')[0]
-    resource_path = '/'.join(('four_d_ct_cost_unrolling','src','configs','backbone_default_configs.json'))
+    resource_path = '/'.join(('src','configs','backbone_default_configs.json'))
     file_path = pkg_resources.resource_filename(package_name, resource_path)
     
     with open(file_path) as file: #"four_d_ct_cost_unrolling/four_d_ct_cost_unrolling/src/configs/backbone_default_configs.json") as file:
@@ -12,7 +12,7 @@ def get_default_backbone_config() -> dict:
 
 def get_default_w_constraints_config() -> dict:
     package_name = __name__.split('.')[0]
-    resource_path = '/'.join(('four_d_ct_cost_unrolling','src','configs','w_constraints_default_configs.json'))
+    resource_path = '/'.join(('src','configs','w_constraints_default_configs.json'))
     file_path = pkg_resources.resource_filename(package_name, resource_path)
 
     with open(file_path) as file: #open("four_d_ct_cost_unrolling/four_d_ct_cost_unrolling/src/configs/w_constraints_default_configs.json") as file:
