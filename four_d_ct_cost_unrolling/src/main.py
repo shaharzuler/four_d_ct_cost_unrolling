1e1c663d3ed81b3079f4882f5b984a0ae07f66d0
shaharzuler <shahar.zuler@gmail.com>
2024-01-11 18:25:04 +0200

    added validation main funcs

--- four_d_ct_cost_unrolling/src/main.py
+++ four_d_ct_cost_unrolling/src/main.py
@@ -60,6 +60,36 @@ def infer_backbone(
     output_path = trainer.infer(args.cuda_device)
     return output_path
     
+
+def validate_backbone(
+    template_image_path:str, unlabeled_image_path:str, 
+    template_LV_seg_path:str, unlabeled_LV_seg_path:str, 
+    template_shell_seg_path:str, unlabeled_shell_seg_path:str, 
+    flows_gt_path:str=None, 
+    error_radial_coordinates_path:str=None,
+    error_circumferential_coordinates_path:str=None,
+    error_longitudinal_coordinates_path:str=None,
+    voxelized_normals_path:str=None,
+    args:Dict=None) -> str: 
+
+    data_sample_args = SegmentationPullerSampleArgs(
+        template_image_path, unlabeled_image_path, 
+        template_LV_seg_path, unlabeled_LV_seg_path, 
+        template_shell_seg_path, unlabeled_shell_seg_path,
+        flows_gt_path, 
+        error_radial_coordinates_path,
+        error_circumferential_coordinates_path, 
+        error_longitudinal_coordinates_path,
+        voxelized_normals_path,
+        args.num_pixels_validate_inside_seg, args.num_pixels_validate_outside_seg)
+    train_set = SegmentationPullerCardioDataset(data_sample_args, sample_type=SegmentationPullerSample, scale_down_by=args.scale_down_by)
+    model = PWC3D(args)
+    loss = get_loss(args)
+    trainer = PullSegmentationMapTrainFramework(train_set, model, loss, args)
+    output_path = trainer.infer(args.cuda_device) #?
+    current_errors = trainer.run_validation()
+    return current_errors
+
 def overfit_w_seg(
     template_image_path:str, unlabeled_image_path:str, 
     template_LV_seg_path:str, unlabeled_LV_seg_path:str, 
@@ -104,6 +134,37 @@ def infer_w_seg(
     trainer = PullSegmentationMapTrainFrameworkWithSegmentation(train_set, model, None, args)
     output_path = trainer.infer(args.cuda_device)
     return output_path
+
+
+def validate_w_seg(
+    template_image_path:str, unlabeled_image_path:str, 
+    template_LV_seg_path:str, unlabeled_LV_seg_path:str, 
+    template_shell_seg_path:str, unlabeled_shell_seg_path:str, 
+    flows_gt_path:str=None, 
+    error_radial_coordinates_path:str=None,
+    error_circumferential_coordinates_path:str=None,
+    error_longitudinal_coordinates_path:str=None,
+    voxelized_normals_path:str=None,
+    args:Dict=None) -> str: 
+
+    data_sample_args = SegmentationPullerSampleArgs(
+        template_image_path, unlabeled_image_path, 
+        template_LV_seg_path, unlabeled_LV_seg_path, 
+        template_shell_seg_path, unlabeled_shell_seg_path,
+        flows_gt_path, 
+        error_radial_coordinates_path,
+        error_circumferential_coordinates_path, 
+        error_longitudinal_coordinates_path,
+        voxelized_normals_path,
+        args.num_pixels_validate_inside_seg, args.num_pixels_validate_outside_seg)
+    train_set = SegmentationPullerCardioDataset(data_sample_args, sample_type=SegmentationPullerSample, scale_down_by=args.scale_down_by)
+    model = PWC3D(args)
+    loss = get_loss(args)
+    trainer = PullSegmentationMapTrainFrameworkWithSegmentation(train_set, model, loss, args)
+    output_path = trainer.infer(args.cuda_device) #?
+    current_errors = trainer.run_validation()
+    return current_errors
+
 
 def overfit_w_constraints(
     template_image_path:str, unlabeled_image_path:str, 
@@ -160,3 +221,37 @@ def infer_w_constraints(
     output_path = trainer.infer(args.cuda_device, save_mask=save_mask)
     return output_path
 
+def validate_w_constraints(
+    template_image_path:str, unlabeled_image_path:str, 
+    template_LV_seg_path:str, unlabeled_LV_seg_path:str, 
+    template_shell_seg_path:str, unlabeled_shell_seg_path:str, 
+    two_d_constraints_path:str, 
+    flows_gt_path:str=None, 
+    error_radial_coordinates_path:str=None,
+    error_circumferential_coordinates_path:str=None,
+    error_longitudinal_coordinates_path:str=None,
+    voxelized_normals_path:str=None,
+    args:Dict=None) -> str:
+
+    data_sample_args = SegmentationPullerSampleWithConstraintsArgs(
+        template_image_path=template_image_path, 
+        unlabeled_image_path=unlabeled_image_path, 
+        template_LV_seg_path=template_LV_seg_path, 
+        unlabeled_LV_seg_path=unlabeled_LV_seg_path, 
+        template_shell_seg_path=template_shell_seg_path, 
+        unlabeled_shell_seg_path=template_shell_seg_path,
+        flows_gt_path=flows_gt_path,
+        error_radial_coordinates_path=error_radial_coordinates_path,
+        error_circumferential_coordinates_path=error_circumferential_coordinates_path, 
+        error_longitudinal_coordinates_path=error_longitudinal_coordinates_path,
+        voxelized_normals_path=voxelized_normals_path,
+        num_pixels_validate_inside_seg=args.num_pixels_validate_inside_seg, 
+        num_pixels_validate_outside_seg=args.num_pixels_validate_outside_seg,
+        two_d_constraints_path=two_d_constraints_path)
+    train_set = SegmentationPullerCardioDatasetWithConstraints(data_sample_args, scale_down_by=args.scale_down_by)
+    model = PWC3Dw2dConstraints(args, train_set.sample.two_d_constraints)
+    loss = get_loss(args)
+    trainer = PullSegmentationMapTrainFrameworkWith2dConstraints(train_set, model, loss, args)
+    output_path = trainer.infer(args.cuda_device) #?
+    current_errors = trainer.run_validation()
+    return current_errors
