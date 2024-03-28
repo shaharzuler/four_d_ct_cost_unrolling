c6ef4930f0ae67121e28ff9bdee8413526968758
shaharzuler <shahar.zuler@gmail.com>
2024-03-28 14:04:50 +0200

    removing constraints net

--- four_d_ct_cost_unrolling/src/models/pwc3d_w_2d_constraints.py
+++ four_d_ct_cost_unrolling/src/models/pwc3d_w_2d_constraints.py
@@ -13,7 +13,6 @@ class PWC3Dw2dConstraints(PWC3D):
         super().__init__(args, upsample, search_range)
         if freeze_backbone:
             self.freeze_all_weights()
-        self.two_d_constraints_network = TwoDConstraintsNetwork(two_d_constraints, num_conv_planes=args.constraints_nn_num_conv_planes)
 
     def freeze_all_weights(self):
         for i in dir(self):
@@ -22,38 +21,9 @@ class PWC3Dw2dConstraints(PWC3D):
                     param.requires_grad = False
                 eval("self."+i).to(torch.device('cpu'))
                 
-
     def forward(self, data:Dict[str,torch.tensor]) -> Dict[str,torch.tensor]: 
         res_dict = super().forward(data)
-        constrained_flows = self.two_d_constraints_network(res_dict["flows_fw"])
         res_dict["unconstrained_flows_fw"] = res_dict["flows_fw"]
-        res_dict["flows_fw"] = constrained_flows 
         res_dict["two_d_constraints"] = data["two_d_constraints"]
         return res_dict 
-        
-class TwoDConstraintsNetwork(nn.Module):
-    def __init__(self, two_d_constraints, num_conv_planes): 
-        super().__init__()
-        self.convs = nn.Sequential(
-            conv(in_planes=6, out_planes=num_conv_planes, kernel_size=3, stride=1, dilation=1, isReLU=True),   
-            conv(in_planes=num_conv_planes, out_planes=3, kernel_size=3, stride=1, dilation=1, isReLU=False) 
-        )
-        self.two_d_constraints = torch.tensor(np.expand_dims(two_d_constraints, 0), requires_grad=False)
-        self.device_ = None
-       
-
-    def forward(self, flows:Tuple[List[torch.Tensor],Dict[str,List]]) -> Tuple[List[torch.Tensor],Dict[str,List]]:
-        return flows #TODO remove this from nn archi
-        if self.device_ is None:
-            self.device_ = flows[0][0].device # assuming all flow pyramid levels are on the same device
-            self.two_d_constraints = self.two_d_constraints.to(self.device_) 
-
-        flows_pyramid, aux_vars = flows
-        constrained_flows = []
-        for flow in flows_pyramid:
-            two_d_constraints_scaled = rescale_flow_tensor(self.two_d_constraints, flow.shape)
-            x = torch.cat([flow, two_d_constraints_scaled], axis=1).float()
-            constrained_flows.append(self.convs(x))
-
-        return constrained_flows, aux_vars
 
